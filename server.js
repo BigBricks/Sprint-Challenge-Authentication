b111677eecb34d463401dff6a6f610599b275f52
unknown <bsa6.23.94@gmail.com>
2018-05-18 11:19:24 -0400

    whoever designed this sprint should have just gave us nothing

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');;
 const cors = require('cors');
 
 const routes = require('./api/routes/routes');
+
+mongoose
+  .connect('mongodb://localhost/sprauthdb')
+  .then(conn => {
+    console.log('\n connected to mongo \n');
+  })
+  .catch(err => console.log('error connecting to mongo', err));
 
 const server = express();
 const corsOptions = {
@@ -15,6 +23,7 @@ server.use(bodyParser.json());
 server.use(cors());
 
 routes(server);
+server.listen(5000, () => console.log('\n api running on 5k \n'));
 
 module.exports = {
   server
